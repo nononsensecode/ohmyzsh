07cfee2977c1637ea46bac617d3b9b2c85f7c491
kaushik <kaushikam@gmail.com>
2023-08-20 11:18:42 +0530

    adding feat function to kgit

--- kgit/kgit.plugin.zsh
+++ kgit/kgit.plugin.zsh
@@ -3,3 +3,8 @@ alias fu='git fetch upstream'
 alias mu='git merge upstream/develop'
 alias dev='git checkout develop'
 alias clone='git clone'
+
+func feat() {
+  git checkout -b "feature/DHP-"$1
+}
+
